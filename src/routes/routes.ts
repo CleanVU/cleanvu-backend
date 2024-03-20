6291d7db2b042182c8773ef9725b7a7eebef3344
jacoblurie29 <jacoblurie29@gmail.com>
2024-03-20 10:44:26 -0500

    Added auth to backend

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -55,60 +55,105 @@ import {
   deleteRequestSchema,
   getRequestByUserIdSchema,
 } from '../schema/request.schema';
+import validateAuth from '../middleware/validateAuth';
+import type { LooseAuthProp } from '@clerk/clerk-sdk-node';
 import type { Express, Request, Response } from 'express';
+
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace Express {
+    interface Request extends LooseAuthProp {}
+  }
+}
 
 const routes = (app: Express) => {
   // healthcheck
   app.get('/healthcheck', (_: Request, res: Response) => res.sendStatus(200));
+  app.get('/auth-healthcheck', validateAuth, (_: Request, res: Response) =>
+    res.sendStatus(200)
+  );
 
   // user endpoints
-  app.get('/api/user', validate(getUserSchema), getUserHandler);
-  app.post('/api/user', validate(createUserSchema), createUserHandler);
-  app.put('/api/user/:userId', validate(updateUserSchema), updateUserHandler);
-  app.delete('/api/user/:userId', validate(deleteUserSchema), deleteUserHandler);
+  app.get('/api/user', validateAuth, validate(getUserSchema), getUserHandler);
+  app.post(
+    '/api/user',
+    validateAuth,
+    validate(createUserSchema),
+    createUserHandler
+  );
+  app.put(
+    '/api/user/:userId',
+    validateAuth,
+    validate(updateUserSchema),
+    updateUserHandler
+  );
+  app.delete(
+    '/api/user/:userId',
+    validateAuth,
+    validate(deleteUserSchema),
+    deleteUserHandler
+  );
 
   // building endpoints
   app.get(
     '/api/building/:buildingId',
+    validateAuth,
     validate(getBuildingSchema),
     getBuildingHandler
   );
   app.post(
     '/api/building',
+    validateAuth,
     validate(createBuildingSchema),
     createBuildingHandler
   );
   app.put(
     '/api/building/:buildingId',
+    validateAuth,
     validate(updateBuildingSchema),
     updateBuildingHandler
   );
   app.delete(
     '/api/building/:buildingId',
+    validateAuth,
     validate(deleteBuildingSchema),
     deleteBuildingHandler
   );
-  app.get('/api/buildings', validate(getBuildingsSchema), getBuildingsHandler);
+  app.get(
+    '/api/buildings',
+    validateAuth,
+    validate(getBuildingsSchema),
+    getBuildingsHandler
+  );
 
   // location endpoints
   app.get(
     '/api/location/:locationId',
+    validateAuth,
     validate(getLocationSchema),
     getLocationHandler
   );
   app.post(
     '/api/location',
+    validateAuth,
     validate(createLocationSchema),
     createLocationHandler
   );
-  app.get('/api/locations', validate(getLocationsSchema), getLocationsHandler);
+  app.get(
+    '/api/locations',
+    validateAuth,
+    validate(getLocationsSchema),
+    getLocationsHandler
+  );
   app.put(
     '/api/location/:locationId',
+    validateAuth,
     validate(updateLocationSchema),
     updateLocationHandler
   );
   app.delete(
     '/api/location/:locationId',
+    validateAuth,
     validate(deleteLocationSchema),
     deleteLocationHandler
   );
@@ -116,23 +161,37 @@ const routes = (app: Express) => {
   // request routes
   app.get(
     '/api/request/:requestId',
+    validateAuth,
     validate(getRequestSchema),
     getRequestHandler
   );
-  app.post('/api/request', validate(createRequestSchema), createRequestHandler);
-  app.get('/api/requests', validate(getRequestsSchema), getRequestsHandler);
+  app.post(
+    '/api/request',
+    validateAuth,
+    validate(createRequestSchema),
+    createRequestHandler
+  );
+  app.get(
+    '/api/requests',
+    validateAuth,
+    validate(getRequestsSchema),
+    getRequestsHandler
+  );
   app.put(
     '/api/request/:requestId',
+    validateAuth,
     validate(updateRequestSchema),
     updateRequestHandler
   );
   app.delete(
     '/api/request/:requestId',
+    validateAuth,
     validate(deleteRequestSchema),
     deleteRequestHandler
   );
   app.get(
     '/api/requests/:userId',
+    validateAuth,
     validate(getRequestByUserIdSchema),
     getRequestByUserIdHandler
   );
